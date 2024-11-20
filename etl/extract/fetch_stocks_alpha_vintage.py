d9d40c45163d878829e2e73eff7a2ef97d51da54
Wafi-Afdi <wafiafdi@gmail.com>
2024-11-20 18:34:45 +0700

    update : fetch_stocks ditambahkan yang montly

--- etl/extract/fetch_stocks_alpha_vintage.py
+++ etl/extract/fetch_stocks_alpha_vintage.py
@@ -9,8 +9,9 @@ load_dotenv()
 # Replace with your Alpha Vantage API key
 ALPHA_VANTAGE_API_KEY = os.getenv('API_KEY_ALPHA')
 ALPHA_VANTAGE_URL = "https://www.alphavantage.co/query"
+COMPANY_SYMBOL="DEVO.L"
 
-def extract_stock_price(symbol="EA"):
+def extract_stock_price_daily(symbol="EA"):
     """
     Extract stock price from Alpha Vantage for the given symbol.
     :param symbol: Stock ticker symbol (default: "EA").
@@ -28,14 +29,39 @@ def extract_stock_price(symbol="EA"):
     data = response.json()
     return data
 
+def extract_stock_price_monthly(symbol="EA"):
+    """
+    Extract stock price from Alpha Vantage for the given symbol.
+    :param symbol: Stock ticker symbol (default: "EA").
+    :return: A dictionary containing stock data.
+    """
+    params = {
+        "function": "TIME_SERIES_MONTHLY",
+        "symbol": symbol,
+        "apikey": ALPHA_VANTAGE_API_KEY,
+        "datatype":"json",
+    }
+
+    response = requests.get(ALPHA_VANTAGE_URL, params=params)
+    data = response.json()
+    return data
+
 
 # Test the extract function
 if __name__ == "__main__":
-    stock_data = extract_stock_price("DEVO.L")
-    if stock_data:
-        print("Extracted Data:", stock_data)
-        output_file = "stocks.json"
+    stock_data_daily = extract_stock_price_daily(COMPANY_SYMBOL)
+    if stock_data_daily:
+        print("Extracted Data:", stock_data_daily)
+        output_file = "stock_daily.json"
         with open(output_file, "w") as file:
-            json.dump(stock_data, file, indent=4)  # indent=4 makes the JSON human-readable
+            json.dump(stock_data_daily, file, indent=4)  # indent=4 makes the JSON human-readable
+    else:
+        print("Failed to extract stock data.")
+    stock_data_monthly = extract_stock_price_monthly(COMPANY_SYMBOL)
+    if stock_data_monthly:
+        print("Stock Data Extracted")
+        output_file = "stock_monthly.json"
+        with open(output_file, "w") as file:
+            json.dump(stock_data_monthly, file, indent=4)  # indent=4 makes the JSON human-readable
     else:
         print("Failed to extract stock data.")
